733b4da24387d06e772c93555f1be3f2aa7c63fe
jcasas5253 <skedabble@gmail.com>
2018-11-13 15:21:33 -0800

    added var for wins losses and player total setting them all to 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
 var compPick = Math.floor(Math.random() * 102) + 19; 
 		console.log("compPick: " + compPick); 
         $("#randomNumber").html(compPick); 
-});
+
 //setting all possible numbers for the gems 1-12
 var number1= Math.floor(Math.random()*11+1)
 var number2= Math.floor(Math.random()*11+1)
 var number3= Math.floor(Math.random()*11+1)
 var number4= Math.floor(Math.random()*11+1)
+
+//setting tallies to 0
+var wins = 0;
+var losses = 0;
+var playerTotal = 0;
+});
