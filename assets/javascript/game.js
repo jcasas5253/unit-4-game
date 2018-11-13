713803345a536e90e81f8c6dc513392e00675c30
jcasas5253 <skedabble@gmail.com>
2018-11-13 15:06:05 -0800

    added var for all possible number outcomes for the gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
+//Random number between 19-120
 $(document).ready(function() {
 
 var compPick = Math.floor(Math.random() * 102) + 19; 
 		console.log("compPick: " + compPick); 
         $("#randomNumber").html(compPick); 
 });
+//setting all possible numbers for the gems 1-12
+var number1= Math.floor(Math.random()*11+1)
+var number2= Math.floor(Math.random()*11+1)
+var number3= Math.floor(Math.random()*11+1)
+var number4= Math.floor(Math.random()*11+1)
